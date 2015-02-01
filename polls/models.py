61a4940ef406c74a9e9ae8916f7561a14041d741
anhtu2 <anhtu>
2015-02-01 10:06:33 -0500

    xong buoc 6

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,14 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):              # __unicode__ on Python 2
         return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     was_published_recently.admin_order_field = 'pub_date'
